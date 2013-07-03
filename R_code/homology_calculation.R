763d199868923d789a29a18b1ab00ffcdaf92e26
Ansgar Wenzel <ansgar@hildebrand.co.uk>
2013-07-03 16:01:35 +0100

    code working
    finished. code working!

--- R_code/homology_calculation.R
+++ R_code/homology_calculation.R
@@ -19,24 +19,61 @@ findX <- function(A){
 #In particular, HNF((HNF(A))^T)
 smith <- function(A){
   HF <- hermiteNF(A)$H
-  S <- hermiteNF(t(HF))$H
+  S <- t(hermiteNF(t(HF))$H)
+  D <- diag(S)
+  for(i in 1:(length(D) - 1)){
+      a <- D[i]
+      b <- D[i + 1]
+      if(a!=1||b!=1){
+        d <- gcd(a,b)
+        if(a==0 || b==0){
+          d <- ifelse(a==0,abs(b),abs(a))
+        }
+        if(d!=0){
+          alpha <- a/d
+          D[i] <- d;
+          D[i + 1] <- -(b * alpha)
+        } else{
+          D[i] <- 0
+          D[i+1] <- 0
+        }
+      }
+    }
+  for(i in 1:length(D)){
+    D[i] <- ifelse(D[i]<0, -D[i], D[i])
+  }
+  diag(S) <- D
   return(S)
 }
+
+i <- i+1
+
+matrix_rank <- function(A){
+  A <- GaussianElimination(A)
+  A <- unique(A)
+  return(nrow(A) - 1)
+}
+
+row_space <- function(B){
+  B <- t(hermiteNF(t(B))$H)
+  return(B)
+}
+
+
 #here is the main function to calculate the homology
 homology <- function(degree, k, degenerate){
-  #boundary_F <- matrix(nrow=12,ncol=6,byrow=T,data=c(1,-1,0,0,1,0,1,-1,-1,0,0,0,-1,1,1,0,0,0,-1,1,0,0,-1,0,0,0,1,-1,0,1,-1,0,1,-1,0,0,0,0,-1,1,0,-1,1,0,-1,1,0,0,0,-1,0,0,1,-1,0,0,0,1,1,-1,0,0,0,-1,-1,1,0,1,0,0,-1,1))#a matrix - Matrix(,sparse=TRUE)
+  #boundary_F <- amatrix(nrow=12,ncol=6,byrow=T,data=c(1,-1,0,0,1,0,1,-1,-1,0,0,0,-1,1,1,0,0,0,-1,1,0,0,-1,0,0,0,1,-1,0,1,-1,0,1,-1,0,0,0,0,-1,1,0,-1,1,0,-1,1,0,0,0,-1,0,0,1,-1,0,0,0,1,1,-1,0,0,0,-1,-1,1,0,1,0,0,-1,1))#a matrix - Matrix(,sparse=TRUE)
   #boundary_G <- matrix(c(-1,0,1,-1,1,0,0,-1,1,1,-1,0,0,1,-1,1,0,-1),ncol=3,nrow=6,byrow=T)#another matrix
   boundary_F <- boundary_matrix(degree + 1, k, degenerate)
   boundary_G <- boundary_matrix(degree, k, degenerate)
-  rho <- rankMatrix(boundary_G)[1]  #first, this calculates the rank of the matrix G. This removes the need to calculate D and Y later.
+  rho <- matrix_rank(boundary_G)  #first, this calculates the rank of the matrix G. This removes the need to calculate D and Y later.
   q <- nrow(boundary_G)
   r <- ncol(boundary_G)
   q_rho <- q - rho
   X <- findX(boundary_G)
   Z <- X[(rho + 1):q, ]       #only take the rows that map to zero (i.e. only the boundaries)
-  B <- GaussianElimination(boundary_F)  #identify the cycles, i.e. remove the boundaries via Gaussian elimination
-  #B <- rref(boundary_F)
-  N <- round(Z %*% ginv(B)) #calculate N. Details in documentation
+  B <- row_space(boundary_F)  #identify the cycles, i.e. remove the boundaries via Gaussian elimination
+  N <- round(B %*% ginv(Z)) #calculate N. Details in documentation
   S <- smith(N)
   Delta <- diag(S)  #Extract the values necessary for the output.
   s <- length(Delta)
