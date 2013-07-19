678c3122c92e8bf6bec2d2dec2043e1b8fef1af6
Ansgar Wenzel <ansgar@ansgar.internal>
2013-07-19 17:44:01 +0100

    new project
    more stuff

--- R_code/S test/S.R
+++ R_code/S test/S.R
@@ -5,9 +5,6 @@ all_combinationsc <- function(k){
 
 all_combinations <- cmpfun(all_combinationsc)
 
-X_squared <- all_combinations(k)
-S_X <- X_squared
-#here, you have to change S_X to define S
 
 check_permutationsc <- function(B){
   C <- unique(B)
@@ -19,3 +16,18 @@ check_permutationsc <- function(B){
 }
 check_permutations <- cmpfun(check_permutationsc)
 
+check_YBc <- function(B,k){
+  LHS <- 
+}
+check_YB <- cmpfun(check_YBc)
+
+k <- 
+X_squared <- all_combinations(k)
+S_X <- X_squared
+#here, you have to change S_X to define S
+
+#then, check that permutations hold:
+permutations <- check_permutations(S_X)
+
+#and check that Yang-Baxter holds
+Yang_Baxter <- check_YB(S_X,k)
