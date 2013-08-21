b91549a52ae23248594bb9fd3459d896cb853558
Ansgar Wenzel <ansgar@hildebrand.co.uk>
2013-08-21 11:58:37 +0100

    code fixed
    test for bijectivity for quandles works. need to include call for g, for
    biquandles.

--- R_code/S test/S.R
+++ R_code/S test/S.R
@@ -19,7 +19,7 @@ check_permutations <- cmpfun(check_permutationsc)
 
 find_S_resultc <- function(x,X,S){
   for(i in 1:nrow(X)){
-    if(all(X[i,]==x)){
+    if(all(X[i, ]==x)){
       match.id <- i
       break
     }
@@ -31,7 +31,7 @@ find_S_result <- cmpfun(find_S_resultc)
 check_YBc <- function(S,k,X){
   for(i in 0:(k - 1)){
     for(j in 1:nrow(S)){
-      LHS <- c(i,S[j,])
+      LHS <- c(i, S[j, ])
       RHS <- LHS
       LHS[1:2] <- find_S_result(LHS[1:2],X,S)
       LHS[2:3] <- find_S_result(LHS[2:3],X,S)
@@ -49,14 +49,14 @@ check_YBc <- function(S,k,X){
 check_YB <- cmpfun(check_YBc)
 
 check_fc <- function(S_X,k,X_squared){
-  A <- S_X[, 1]
-  F_A <- X_squared[, 2]
-  S <- unique(cbind(A,F_A))
-  if(nrow(S)!=k){
-    return(FALSE)
-  } else{
-    return(TRUE)
+  S <- unique(cbind(X_squared[, 1], S_X[, 2]))
+  for(i in 0:(k-1)){
+    test_s <- matrix(S[S[, 1]==i],ncol=2,byrow=FALSE)
+    if(nrow(test_s)!=k){
+      return(FALSE)
+    }
   }
+  return(TRUE)
 }
 
 check_f <- cmpfun(check_fc)
@@ -65,8 +65,8 @@ k <- 3#define k for Z_k (or X_k) where S lives in...
 X_squared <- all_combinations(k)
 S_X <- X_squared[, 2:1]
 #here, you have to change S_X to define S
- S_X[, 2] <- up_action(X_squared[, 1], S_X[, 1], k)
-X_squared[, 2] <- down_action(X_squared[, 1], S_X[, 1], k)
+S_X[, 2] <- up_action(X_squared[, 1], S_X[, 1], k)
+X_squared[, 2] <- down_action(S_X[, 1], X_squared[, 1], k)
 
 
 #then, check that permutations hold:
@@ -77,4 +77,4 @@ permutations_f <- check_f(S_X,k,X_squared)
 Yang_Baxter <- check_YB(S_X,k,X_squared)
 
 
-print(paste0("the permutations check holds ", permutations, " and ",permutations_f, " and the Yang-Baxter check holds ", Yang_Baxter, "."))
+print(paste0("the permutations check holds ", permutations_S, " and ",permutations_f, " and the Yang-Baxter check holds ", Yang_Baxter, "."))
